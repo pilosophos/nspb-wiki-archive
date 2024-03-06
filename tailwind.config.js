e02f9fff36efe181d69f8dd9f30b69b3eef428ff
pilosophos <48043580+pilosophos@users.noreply.github.com>
2024-03-05 19:59:07 -0800

    make prose wider

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,40 +7,18 @@ module.exports = {
     './*.md',
     './*.html',
   ],
-  safelist: [
-    "copyable-hover", // used for the resume-clipboard.js module only
-    "copyable-active", // used for the resume-clipboard.js module only
-  ],
   theme: {
     extend: {
-      container: {
-        center: true,
-        padding: {
-          DEFAULT: '1.5rem',
+      typography: {
+        DEFAULT: {
+          css: {
+            maxWidth: '100ch',
+          }
         }
-      },
+      }
     },
   },
   plugins: [
     require('@tailwindcss/typography'),
-    function({ addComponents }) {
-      addComponents({
-        '.container': {
-          maxWidth: '100%',
-          '@screen sm': {
-            maxWidth: '540px',
-          },
-          '@screen md': {
-            maxWidth: '720px',
-          },
-          '@screen lg': {
-            maxWidth: '960px',
-          },
-          '@screen xl': {
-            maxWidth: '1140px',
-          },
-        }
-      })
-    },
   ]
 }
